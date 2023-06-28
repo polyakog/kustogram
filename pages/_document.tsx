4aae558da1dd3f47a9352c0a0f48f1447f2c298f
polyakog <gpolyakov77@gmail.com>
2023-06-28 19:39:14 +0400

    refac: _document format

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,49 +4,49 @@ import Document, {
     Html,
     Main,
     NextScript,
-} from "next/document";
-import { ServerStyleSheet } from "styled-components";
-
-class MyDocument extends Document {
+  } from "next/document";
+  import {ServerStyleSheet} from "styled-components";
+  
+  class MyDocument extends Document {
     static async getInitialProps(ctx: DocumentContext) {
-        const sheet = new ServerStyleSheet();
-
-        try {
-            ctx.renderPage({
-                enhanceApp: (App) => (props) => sheet.collectStyles(<App {...props} />),
-            });
-
-            const initialProps = await Document.getInitialProps(ctx);
-            return {
-                ...initialProps,
-                styles: (
-                    <>
-                        {initialProps.styles}
-                        {sheet.getStyleElement()}
-                    </>
-                ),
-            };
-        } finally {
-            sheet.seal();
-        }
+      const sheet = new ServerStyleSheet();
+  
+      try {
+        ctx.renderPage({
+          enhanceApp: (App) => (props) => sheet.collectStyles(<App {...props} />),
+        });
+  
+        const initialProps = await Document.getInitialProps(ctx);
+        return {
+          ...initialProps,
+          styles: (
+            <>
+              {initialProps.styles}
+              {sheet.getStyleElement()}
+            </>
+          ),
+        };
+      } finally {
+        sheet.seal();
+      }
     }
-
+  
     render() {
-        return (
-            <Html>
-                <Head>
-                    {
-                        // @ts-ignore
-                        this.props.styleTags
-                    }
-                </Head>
-                <body>
-                <Main />
-                <NextScript />
-                </body>
-            </Html>
-        );
+      return (
+        <Html>
+          <Head>
+            {
+              // @ts-ignore
+              this.props.styleTags
+            }
+          </Head>
+          <body>
+          <Main/>
+          <NextScript/>
+          </body>
+        </Html>
+      );
     }
-}
-
-export default MyDocument;
+  }
+  
+  export default MyDocument;

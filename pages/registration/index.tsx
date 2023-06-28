d352c9b1f389c4389d3076d18c8b4f8a29f9fd5e
polyakog <gpolyakov77@gmail.com>
2023-06-28 20:46:09 +0400

    refac: registration page

--- pages/registration/index.tsx
+++ pages/registration/index.tsx
@@ -1,29 +1,29 @@
-import React from "react"
-import {Formik} from "formik"
-import showPasswordBtn from "../../public/icons/eye-outline.svg"
-import hidePasswordBtn from "../../public/icons/eye-off-outline.svg"
-import {getLayout} from "../../components/Layout/BaseLayout/BaseLayout"
-import {useShowPassword} from "assets/hooks/useShowPassword"
-import {validateRegistration} from "../../utils/validateRegistraition"
-import AuthIcons from "../../components/Wrappers/Auth/AuthIcons"
-import {WrapperContainerAuth} from "../../components/Wrappers/Auth/WrapperContainerAuth"
-import {Button, ThemeButton} from "../../components/Button/Button"
-import {FormikLabel} from "../../components/Formik/FormikLabel"
+import React from 'react';
+import {getLayout} from "../../components/Layout/BaseLayout/BaseLayout";
+import {Button, ThemeButton} from "../../components/Button/Button";
+import {WrapperContainerAuth} from "../../components/Wrappers/Auth/WrapperContainerAuth";
+import {Formik} from "formik";
+import AuthIcons from "../../components/Wrappers/Auth/AuthIcons";
 import {
   StyledAuthForm,
-  StyledContainerAuth, StyledShowPasswordBtn, StyledSignIn,
-  StyledSignInWrapper,
-  StyledText
-} from "../../styles/styledComponents/auth/FormikAuth.styled"
-import {useRegistrationMutation} from "store/api/auth/authApi"
-import {FormValueRegistration, ResetForm} from "components/Formik/types"
+  StyledContainerAuth,
+  StyledShowPasswordBtn, StyledSignIn,
+  StyledSignInWrapper, StyledText
+} from "../../styles/styledComponents/auth/FormikAuth.styled";
+import {useShowPassword} from "../../assets/hooks/useShowPassword";
+import {useLoginMutation} from "../../store/api/auth/authApi";
+import {FormValueLogin, ResetForm} from "../../components/Formik/types";
+import {FormikLabel} from "../../components/Formik/FormikLabel";
+import showPasswordBtn from "../../public/icons/eye-outline.svg";
+import hidePasswordBtn from "../../public/icons/eye-off-outline.svg";
+import {validateLogin} from "../../utils/validateLogin";
 
-export default function Registration() {
+
+const Login = () => {
+
   const {
     passwordType,
-    passwordConfirmationType,
     showPassword,
-    showPasswordConfirmation
   } =
     useShowPassword()
 
@@ -35,20 +35,17 @@ export default function Registration() {
     loginOrEmail: ""
   }
 
-  const [registrationHandler] = useRegistrationMutation()
+  const [loginHandler] = useLoginMutation()
 
-
-  const handleSubmit = async (values: FormValueRegistration, {resetForm}: ResetForm) => {
+  const handleSubmit = async (values: FormValueLogin, {resetForm}: ResetForm) => {
     const data = {
-      email: values.email,
+      loginOrEmail: values.loginOrEmail,
       password: values.password,
-      login: values.username
     }
     try {
-      await registrationHandler(data)
+      await loginHandler(data)
       resetForm()
     } catch (error) {
-      console.log(error)
     }
   }
 
@@ -58,33 +55,18 @@ export default function Registration() {
         <AuthIcons/>
         <Formik
           initialValues={initialAuthValues}
-          validationSchema={validateRegistration}
+          validationSchema={validateLogin}
           onSubmit={handleSubmit}
         >
-          {({
-              errors,
-              touched,
-              values,
-              setFieldValue
-            }) => (
+          {({errors, touched, values, setFieldValue}) => (
             <StyledAuthForm>
               <FormikLabel
-                name="username"
-                onChange={(e) => setFieldValue("username", e)}
-                value={values.username}
+                name="loginOrEmail"
+                onChange={(e) => setFieldValue("loginOrEmail", e)}
+                value={values.loginOrEmail}
                 type={"text"}
-                title={"Username"}
-                border={errors.username?.length && touched.username ? "red" : "white"}
-                errors={errors}
-                touched={touched}
-              />
-              <FormikLabel
-                name="email"
-                onChange={(e) => setFieldValue("email", e)}
-                value={values.email}
-                type={"email"}
-                title={"email"}
-                border={errors.email?.length && touched.email ? "red" : "white"}
+                title={"login or Email"}
+                border={errors.loginOrEmail?.length && touched.loginOrEmail ? "red" : "white"}
                 errors={errors}
                 touched={touched}
               />
@@ -105,40 +87,24 @@ export default function Registration() {
                   onClick={() => showPassword()}
                 />
               </FormikLabel>
-              <FormikLabel
-                id="pass"
-                name="passwordConfirmation"
-                onChange={(e) => setFieldValue("passwordConfirmation", e)}
-                value={values.passwordConfirmation}
-                type={passwordConfirmationType}
-                title={"Password confirmation"}
-                border={
-                  errors.passwordConfirmation?.length && touched.passwordConfirmation
-                    ? "red"
-                    : "white"
-                }
-                errors={errors}
-                touched={touched}
-              >
-                <StyledShowPasswordBtn
-                  alt="show password"
-                  src={passwordConfirmationType === "password" ? showPasswordBtn : hidePasswordBtn}
-                  onClick={() => showPasswordConfirmation()}
-                />
-              </FormikLabel>
               <Button theme={ThemeButton.PRIMARY} type="submit">
-                Sign up
+                Sign in
               </Button>
             </StyledAuthForm>
           )}
         </Formik>
         <StyledSignInWrapper>
           <StyledText>Don’t have an account?</StyledText>
-          <StyledSignIn href="/login">Sign in</StyledSignIn>
+          <StyledSignIn href="/registration">Sign up</StyledSignIn>
         </StyledSignInWrapper>
       </WrapperContainerAuth>
     </StyledContainerAuth>
   )
 }
 
-Registration.getLayout = getLayout
+Login.getLayout = getLayout
+export default Login;
+
+
+
+

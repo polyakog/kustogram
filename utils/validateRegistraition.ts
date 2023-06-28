f90e83508bcc43257cef2b67c5824235bc3256e1
polyakog <gpolyakov77@gmail.com>
2023-06-28 19:13:31 +0400

    refac: chagne validateRegistration - function name

--- utils/validateRegistraition.ts
+++ utils/validateRegistraition.ts
@@ -1,6 +1,6 @@
 import * as Yup from "yup";
 
-export const SignupSchema = Yup.object().shape({
+export const validateRegistration = Yup.object().shape({
   username: Yup.string()
     .min(6, "Too Short!")
     .max(30, "Too Long!")

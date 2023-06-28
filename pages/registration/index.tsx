13a90bdac198daf7c6717abe2f964e65e52e6b1f
polyakog <gpolyakov77@gmail.com>
2023-06-28 14:14:59 +0400

    feat: change registration

--- pages/registration/index.tsx
+++ pages/registration/index.tsx
@@ -1,142 +1,132 @@
-import React, { useState } from "react";
-import { Formik } from "formik";
-import showPasswordBtn from "../../assets/svg/eye-outline.svg";
-import hidePasswordBtn from "../../assets/svg/eye-off-outline.svg";
-import googleIcon from "../../assets/svg//google-icon.svg";
-import githubIcon from "../../assets/svg/github-icon.svg";
-import { SignupSchema } from "utils/registrationValidation";
+import React from "react"
+import {Formik} from "formik"
+import showPasswordBtn from "../../public/icons/eye-outline.svg"
+import hidePasswordBtn from "../../public/icons/eye-off-outline.svg"
 import {
-  StyledBtn,
-  StyledContainer,
   StyledErrorMsg,
-  StyledField,
   StyledForm,
   StyledShowPasswordBtn,
   StyledSignIn,
   StyledSignInWrapper,
-  StyledSocialMediaIcon,
-  StyledSocialMediaWrapper,
-  StyledText,
-  StyledTitle,
-} from "styles/styles";
-
+  StyledText
+} from "styles/styles"
+import {getLayout} from "../../components/Layout/BaseLayout/BaseLayout"
+import {useShowPassword} from "assets/hooks/useShowPassword"
+import {SignupSchema} from "../../utils/validateRegistraition";
+import FormikLabel from "../../components/Formik/FormikLabel";
+import {FormikField} from "../../components/Formik/FormikField";
+import AuthIcons from "../../components/Wrappers/Auth/AuthIcons";
+import {WrapperContainerAuth} from "../../components/Wrappers/Auth/WrapperContainerAuth";
+import {StyledContainerAuth} from "../login";
+import {Button, ThemeButton} from "../../components/Button/ui/Button";
 
 export default function Registration() {
-  const [passwordType, setPasswordType] = useState("password");
-  const [passwordConfirmationType, setPasswordConfirmationType] =
-    useState("password");
-
-  const showPasswordHandler = () => {
-    if (passwordType === "text") {
-      setPasswordType("password");
-    } else {
-      setPasswordType("text");
-    }
-  };
-  const showPasswordConfirmationHandler = () => {
-    if (passwordConfirmationType === "text") {
-      setPasswordConfirmationType("password");
-    } else {
-      setPasswordConfirmationType("text");
-    }
-  };
+  const {passwordType, passwordConfirmationType, showPassword, showPasswordConfirmation} =
+    useShowPassword()
 
   return (
-    <StyledContainer>
-      <StyledTitle>Sign Up</StyledTitle>
-      <StyledSocialMediaWrapper>
-        <StyledSocialMediaIcon alt="google-icon" src={googleIcon} />
-        <StyledSocialMediaIcon alt="github-icon" src={githubIcon} />
-      </StyledSocialMediaWrapper>
-      <Formik
-        initialValues={{
-          username: "",
-          password: "",
-          passwordConfirmation: "",
-          email: "",
-        }}
-        validationSchema={SignupSchema}
-        onSubmit={async (values, { resetForm }) => {
-          console.log(values);
-          const data = {
-            email: values.email,
-            password: values.password,
-            username: values.username,
-          };
+    <StyledContainerAuth>
+      <WrapperContainerAuth title={'Sing In'}>
+        <AuthIcons/>
+        <Formik
+          initialValues={{
+            username: "",
+            password: "",
+            passwordConfirmation: "",
+            email: ""
+          }}
+          validationSchema={SignupSchema}
+          onSubmit={async (values, {resetForm}) => {
+            const data = {
+              email: values.email,
+              password: values.password,
+              login: values.username
+            }
 
-          try {
-            await fetch("https://jsonplaceholder.typicode.com/users", {
-              method: "POST",
-              body: JSON.stringify(data),
-            }).then(() => console.log("otpravleno"));
-            resetForm();
-          } catch {
-            console.log("err");
-          }
-        }}
-      >
-        {({ errors, touched }) => (
-          <StyledForm>
-            <label>
-              Username
-              <StyledField
-                name="username"
-                // border={errors.username?.length ? "red" : "white"}
-              />
-              {errors.username && touched.username ? (
-                <StyledErrorMsg>{errors.username}</StyledErrorMsg>
-              ) : null}
-            </label>
-            <label id="pass">
-              Password
-              <StyledField name="password" type={passwordType} />
-              <StyledShowPasswordBtn
-                alt="show password"
-                src={
-                  passwordType === "password"
-                    ? showPasswordBtn
-                    : hidePasswordBtn
+            try {
+              await fetch("https://calypso-one.vercel.app/auth/registration", {
+                method: "POST",
+                body: JSON.stringify(data),
+                headers: {
+                  "Content-Type": "application/json"
                 }
-                onClick={() => showPasswordHandler()}
-              />
-              {errors.password && touched.password ? (
-                <StyledErrorMsg>{errors.password}</StyledErrorMsg>
-              ) : null}
-            </label>
-            <label id="pass">
-              Password confirmation
-              <StyledShowPasswordBtn
-                alt="show password"
-                src={
-                  passwordConfirmationType === "password"
-                    ? showPasswordBtn
-                    : hidePasswordBtn
-                }
-                onClick={() => showPasswordConfirmationHandler()}
-              />
-              <StyledField
-                name="passwordConfirmation"
-                type={passwordConfirmationType}
-              />
-              {errors.passwordConfirmation && touched.passwordConfirmation ? (
-                <StyledErrorMsg>{errors.passwordConfirmation}</StyledErrorMsg>
-              ) : null}
-            </label>
-            <label>
-              Email
-              <StyledField name="email" type="email" />
-              {errors.email && touched.email ? (
-                <StyledErrorMsg>{errors.email}</StyledErrorMsg>
-              ) : null}
-            </label>
-            <StyledBtn type="submit">Sign Up</StyledBtn>
-          </StyledForm>
-        )}
-      </Formik>
-      <StyledSignInWrapper>
-        <StyledText>Do you have an account?</StyledText>
-        <StyledSignIn href="/login">Sign in</StyledSignIn>
-      </StyledSignInWrapper>
-    </StyledContainer>
-  );
+              }).then(() => console.log("otpravleno"))
+              resetForm()
+            } catch {
+              console.log("err")
+            }
+          }}
+        >
+          {({errors, touched}) => (
+            <StyledForm>
+              <FormikLabel>
+                Username
+                <FormikField
+                  name="username"
+                  border={errors.username?.length && touched.username ? "red" : "white"}
+                />
+                {errors.username && touched.username ? (
+                  <StyledErrorMsg>{errors.username}</StyledErrorMsg>
+                ) : null}
+              </FormikLabel>
+              <FormikLabel id="pass">
+                Password
+                <FormikField
+                  name="password"
+                  type={passwordType}
+                  border={errors.password?.length && touched.password ? "red" : "white"}
+                />
+                <StyledShowPasswordBtn
+                  alt="show password"
+                  src={passwordType === "password" ? showPasswordBtn : hidePasswordBtn}
+                  onClick={() => showPassword()}
+                />
+                {errors.password && touched.password ? (
+                  <StyledErrorMsg>{errors.password}</StyledErrorMsg>
+                ) : null}
+              </FormikLabel>
+              <FormikLabel id="pass">
+                Password confirmation
+                <StyledShowPasswordBtn
+                  alt="show password"
+                  src={passwordConfirmationType === "password" ? showPasswordBtn : hidePasswordBtn}
+                  onClick={() => showPasswordConfirmation()}
+                />
+                <FormikField
+                  name="passwordConfirmation"
+                  type={passwordConfirmationType}
+                  border={
+                    errors.passwordConfirmation?.length && touched.passwordConfirmation
+                      ? "red"
+                      : "white"
+                  }
+                />
+                {errors.passwordConfirmation && touched.passwordConfirmation ? (
+                  <StyledErrorMsg>{errors.passwordConfirmation}</StyledErrorMsg>
+                ) : null}
+              </FormikLabel>
+              <FormikLabel>
+                Email
+                <FormikField
+                  name="email"
+                  type="email"
+                  border={errors.email?.length && touched.email ? "red" : "white"}
+                />
+                {errors.email && touched.email ? (
+                  <StyledErrorMsg>{errors.email}</StyledErrorMsg>
+                ) : null}
+              </FormikLabel>
+              <Button theme={ThemeButton.PRIMARY} type="submit">Sign Up</Button>
+            </StyledForm>
+          )}
+        </Formik>
+        <StyledSignInWrapper>
+          <StyledText>Don’t have an account?</StyledText>
+          <StyledSignIn href="/login">Sign up</StyledSignIn>
+        </StyledSignInWrapper>
+      </WrapperContainerAuth>
+    </StyledContainerAuth>
+  )
 }
+
+Registration.getLayout = getLayout

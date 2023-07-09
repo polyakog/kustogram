cb25e62c99da9e7fe923c5c0d93ffbf5f12953f8
Gennadij <gpolyakov77@gmail.com>
2023-07-09 20:47:55 +0400

    feat: add useWindowSize hook in 404.tsx

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,18 +1,41 @@
 import Image from "next/image";
 import {getLayout} from "../common/components/Layout/BaseLayout/BaseLayout";
 import Link from "next/link";
+import { useWindowSize } from "common/hooks/useWindowSize";
+import errorImage from "public/img/404.svg"
+import { StyledSignInWrapper, StyledText } from "styles/styledComponents/auth/FormikAuth.styled";
+import { useTranslation } from "react-i18next";
+import { GetStaticPropsContext } from "next";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+import config from 'next-i18next.config.js'
+
+
+export async function getStaticProps(context: GetStaticPropsContext) {
+  const {locale} = context as any
+  return {
+    props: {
+      ...(await serverSideTranslations(locale, ["common"], config)),
+    }
+  }
+}
 
 const NotFound = () => {
+  const { width, height } = useWindowSize()
+  const {t} = useTranslation()
   return <div>
+    <StyledSignInWrapper> 
+          {/* <StyledText>{t("error_404")}</StyledText> */}
+        </StyledSignInWrapper> 
+    
     <Image
-    src="/404.svg"
-    alt="Next.js Logo"
-    width={1280}
-    height={720}
+    src={errorImage}
+    alt="404 error"
+    width={width? width:1280}
+    height={height? height:720}
     priority
   />
     <div>
-      <Link href={'/'}>Back Main</Link>
+      <Link href={'/'}>Back to Main</Link>
     </div>
 
   </div>

0bf839b38ab20d2481755e3bc2083c57362d7a8b
Gennadij <gpolyakov77@gmail.com>
2023-07-11 21:23:01 +0400

    faet: add my profile and profile settings

--- pages/profile/index.tsx
+++ pages/profile/index.tsx
@@ -1,12 +1,230 @@
-import React from 'react';
-import {getLayout} from '../../common/components/Layout/PageLayout/PageLayout';
+import React, { useState, useEffect } from "react";
+import { getLayout } from "../../common/components/Layout/PageLayout/PageLayout";
+import Image from "next/image";
+import { useProfileQuery } from "assets/store/api/profile/profileApi";
+import styled from "styled-components";
+import { baseTheme } from "styles/styledComponents/theme";
+import { Button } from "common/components/Button/Button";
+import { ThemeButton } from "common/enums/themeButton";
+import { useRouter } from "next/router";
+import { useTranslation } from "react-i18next";
+import { Path } from "common/enums/path";
+import { useWindowSize } from "common/hooks/useWindowSize";
 
 const MyProfile = () => {
+  const serverAvatar: string = "";
+  const avatar = serverAvatar !== "" ? serverAvatar : "/img/icons/avatar.svg";
+  const { data } = useProfileQuery();
+
+  const { width, height } = useWindowSize();
+  const [isVisible, setIsVisible] = useState(true);
+  const router = useRouter();
+  const { t } = useTranslation();
+
+  const handleClick = () => {
+    router.push(Path.PROFILE_SETTINGS);
+  };
+
+  useEffect(() => {
+    if (width) {
+      if (width < 880) {
+        setIsVisible(false);
+      } else {
+        setIsVisible(true);
+      }
+    }
+  }, [width]);
+
   return (
-    <div>
-      My Profile
-    </div>
+    <ProfileWrapper>
+      <HeaderStyle>
+        {isVisible && (
+          <BlockButton>
+            <Button
+              theme={ThemeButton.SECONDARY}
+              type="button"
+              width={"auto"}
+              style={{ padding: "6px 24px", gap: "10px" }}
+              onClick={handleClick}
+            >
+              Profile Settings
+            </Button>
+          </BlockButton>
+        )}
+
+        <IconBlock>
+          <Image
+            src={avatar}
+            width={width ? (width < 790 ? 72 : 204) : 204}
+            height={width ? (width < 790 ? 72 : 204) : 204}
+            alt={"avatar"}
+            style={{ maxWidth: "204px", maxHeight: "204px" }}
+          />
+        </IconBlock>
+        <UserNameStyle> URLProfile</UserNameStyle>
+
+        <InfoBlock>
+          <FolowBlock>
+            <div>
+              <div>2 218</div>
+              <div>Following</div>
+            </div>
+            <div>
+              <div>2 358</div>
+              <div>Followers</div>
+            </div>
+            <div>
+              <div>2 358</div>
+              <div>Publications</div>
+            </div>
+          </FolowBlock>
+
+          <AboutMeBlock>
+            <AboutMeText>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+              incididunt. laboris nisi ut aliquip ex ea commodo consequat.
+            </AboutMeText>
+          </AboutMeBlock>
+        </InfoBlock>
+      </HeaderStyle>
+      <PhotosBlock>
+        <PhotoStyle>Photo</PhotoStyle>
+        <PhotoStyle>Photo</PhotoStyle>
+        <PhotoStyle>Photo</PhotoStyle>
+        <PhotoStyle>Photo</PhotoStyle>
+        <PhotoStyle>Photo</PhotoStyle>
+        <PhotoStyle>Photo</PhotoStyle>
+        <PhotoStyle>Photo</PhotoStyle>
+      </PhotosBlock>
+    </ProfileWrapper>
   );
 };
-MyProfile.getLayout = getLayout
+MyProfile.getLayout = getLayout;
 export default MyProfile;
+
+const ProfileWrapper = styled.div`
+  position: relative;
+`;
+
+const BlockButton = styled.div`
+  position: absolute;
+  right: 64px;
+  top: 0px;
+`;
+
+const HeaderStyle = styled.div`
+  display: inline-flex;
+  gap: 40px;
+  align-content: center;
+  width: auto;
+`;
+
+const IconBlock = styled.div`
+  border-radius: 50%;
+  background: ${baseTheme.colors.dark[100]};
+  max-width: 204px;
+  max-height: 204px;
+
+  @media (max-width: 790px) {
+    margin-top: 0px;
+    max-width: 72px;
+    max-height: 72px;
+  }
+`;
+
+const UserNameStyle = styled.div`
+  position: absolute;
+  left: 242px;
+  top: 0px;
+
+  color: ${baseTheme.colors.light[100]};
+  font-family: Inter;
+  font-size: 20px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 36px;
+
+  @media (max-width: 790px) {
+    position: absolute;
+    left: 0px;
+    top: 82px;
+    font-family: Inter;
+    font-size: 16px;
+    line-height: 24px;
+  }
+`;
+const InfoBlock = styled.div`
+  margin-top: 151px;
+
+  @media (max-width: 790px) {
+    margin-top: 108px;
+  }
+`;
+
+const FolowBlock = styled.div`
+  display: grid;
+  margin-top: -90px;
+  grid-template-columns: 1fr 1fr 1fr;
+
+  font-family: Inter;
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 24px;
+
+  @media (max-width: 790px) {
+    text-align: center;
+    font-family: Inter;
+    font-size: 12px;
+    font-weight: 600;
+    line-height: 16px;
+  }
+`;
+
+const AboutMeBlock = styled.div`
+  display: flex;
+  width: 100%;
+  flex-direction: column;
+  flex-shrink: 0;
+  margin-top: 24px;
+
+  @media (max-width: 790px) {
+    margin-top: 80px;
+    margin-left: -110px;
+    width: auto;
+    display: flex;
+    max-width: 330px;
+    flex-direction: column;
+    flex-shrink: 0;
+  }
+`;
+
+const AboutMeText = styled.p`
+  font-family: Inter;
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 24px;
+  color: ${baseTheme.colors.light[100]};
+
+  @media (max-width: 790px) {
+    font-size: 14px;
+  }
+`;
+
+const PhotosBlock = styled.div`
+  display: inline-flex;
+  flex-wrap: wrap;
+  padding-top: 24px;
+  justify-content: start;
+  align-items: center;
+  gap: 10px;
+`;
+
+const PhotoStyle = styled.div`
+  width: 234px;
+  height: 228px;
+  flex-shrink: 0;
+  border-radius: 2px;
+  background: url(<path-to-image>), lightgray 50% / cover no-repeat;
+`;
--- pages/profile/settings/index.tsx
+++ pages/profile/settings/index.tsx
@@ -1,70 +1,71 @@
-import React from 'react';
-import {Formik} from 'formik';
-import styled from 'styled-components';
-import {useRouter} from 'next/router';
-import {useSetProfileMutation} from '../../../assets/store/api/auth/authApi';
-import {baseTheme} from '../../../styles/styledComponents/theme';
-import {FormValueProfile, ResetForm} from '../../../common/components/Formik/types';
-import {Button} from '../../../common/components/Button/Button';
-import {FormikLabel} from '../../../common/components/Formik/FormikLabel';
-import {validateProfile} from '../../../common/utils/validateProfile';
-import {SettingsPageWrapper} from '../../../features/settings/SettingsPageWrapper';
-import {getLayout} from '../../../common/components/Layout/SettingsLayout/SettingsLayout';
-import {useProfileQuery} from '../../../assets/store/api/profile/profileApi';
-import {ThemeButton} from "../../../common/enums/themeButton";
+import React, { useLayoutEffect } from "react";
+import { Form, Formik } from "formik";
+import styled from "styled-components";
+import { useRouter } from "next/router";
+import { useSetProfileMutation } from "../../../assets/store/api/auth/authApi";
+import { baseTheme } from "../../../styles/styledComponents/theme";
+import { FormValueProfile, ResetForm } from "../../../common/components/Formik/types";
+import { Button } from "../../../common/components/Button/Button";
+import { FormikLabel } from "../../../common/components/Formik/FormikLabel";
+import { validateProfile } from "../../../common/utils/validateProfile";
+import { SettingsPageWrapper } from "../../../features/settings/SettingsPageWrapper";
+import { getLayout } from "../../../common/components/Layout/SettingsLayout/SettingsLayout";
+import {
+  useProfileQuery,
+  useSaveProfileInfoMutation
+} from "../../../assets/store/api/profile/profileApi";
+import { ThemeButton } from "../../../common/enums/themeButton";
 
 const GeneralInformation = () => {
-  // const serverAvatar: string = ''
-  // const avatar = serverAvatar !== '' ? serverAvatar : '/icons/avatar.svg'
+  const serverAvatar: string = "";
+  const avatar = serverAvatar !== "" ? serverAvatar : "/img/icons/avatar.svg";
 
-  const [setProfileHandler] = useSetProfileMutation()
-  const { data } = useProfileQuery()
+  useLayoutEffect(() => {}, []);
 
-  console.log('data', data)
+  const [saveProfileInfoHandler] = useSaveProfileInfoMutation();
+  const { data } = useProfileQuery();
+  console.log(data);
 
-  const router = useRouter()
-  const {login} = router.query
+  // const router = useRouter();
+  // const { profile } = router.query;
 
   const initialAuthValues = {
-    username: login as string,
-    firstname: '',
-    lastname: '',
-    birthday: '',
-    city: '',
-    aboutMe: ''
-  }
+    username: data?.login || "",
+    firstname: data?.firstName || "",
+    lastname: data?.lastName || "",
+    birthday: data?.dateOfBirthday || "",
+    city: data?.city || "",
+    aboutMe: data?.userInfo || ""
+  };
 
-  const handleSubmit = async (values: FormValueProfile, {resetForm}: ResetForm) => {
+  const handleSubmit = async (values: FormValueProfile, { resetForm }: ResetForm) => {
+    const date = values.birthday.split("-").reverse().join("-");
+    console.log(date);
     const data = {
-      username: values.username,
-      firstname: values.firstname,
-      lastname: values.lastname,
-      birthday: values.birthday,
+      login: values.username,
+      firstName: values.firstname,
+      lastName: values.lastname,
+      dateOfBirthday: date,
       city: values.city,
-      aboutMe: values.aboutMe
-    }
+      userInfo: values.aboutMe
+    };
     try {
-      console.log(values.aboutMe)
+      await saveProfileInfoHandler(data);
+    } catch (error) {}
+  };
 
-      await setProfileHandler(data)
-      resetForm()
-    } catch (error) {
-    }
-  }
 
-  const serverAvatar: string = ''
-  const avatar = serverAvatar !== '' ? serverAvatar : '/img/icons/avatar.svg'
 
   return (
     <SettingsPageWrapper>
       <StyledContent>
         <StyledAvatarBlock>
           <IconBlock>
-            <img src={avatar} alt="Avatar"/>
+            <img src={avatar} alt="Avatar" />
             {/*<Image src={avatar} alt={"Avatar"} width={192} height={192}/>*/}
           </IconBlock>
 
-          <Button theme={ThemeButton.OUTLINED} width={'100%'}>
+          <Button theme={ThemeButton.OUTLINED} width={"100%"}>
             Add a Profile Photo
           </Button>
         </StyledAvatarBlock>
@@ -73,77 +74,77 @@ const GeneralInformation = () => {
           validationSchema={validateProfile}
           onSubmit={handleSubmit}
         >
-          {({errors, touched, values, setFieldValue}) => (
+          {({ errors, touched, values, setFieldValue }) => (
             <StyledProfileForm>
               <FormikLabel
                 name="username"
-                onChange={(e) => setFieldValue('username', e)}
+                onChange={(e) => setFieldValue("username", e)}
                 value={values.username}
-                type={'text'}
-                title={'Username'}
-                border={errors.username?.length && touched.username ? 'red' : 'white'}
+                type={"text"}
+                title={"Username"}
+                border={errors.username?.length && touched.username ? "red" : "white"}
                 errors={errors}
                 touched={touched}
-                width={'100%'}
+                width={"100%"}
               />
               <FormikLabel
                 name="firstname"
-                onChange={(e) => setFieldValue('firstname', e)}
+                onChange={(e) => setFieldValue("firstname", e)}
                 value={values.firstname}
-                type={'text'}
-                title={'First Name'}
-                border={errors.firstname?.length && touched.firstname ? 'red' : 'white'}
+                type={"text"}
+                title={"First Name"}
+                border={errors.firstname?.length && touched.firstname ? "red" : "white"}
                 errors={errors}
                 touched={touched}
-                width={'100%'}
+                width={"100%"}
               />
               <FormikLabel
                 name="lastname"
-                onChange={(e) => setFieldValue('lastname', e)}
+                onChange={(e) => setFieldValue("lastname", e)}
                 value={values.lastname}
-                type={'text'}
-                title={'Last Name'}
-                border={errors.lastname?.length && touched.lastname ? 'red' : 'white'}
+                type={"text"}
+                title={"Last Name"}
+                border={errors.lastname?.length && touched.lastname ? "red" : "white"}
                 errors={errors}
                 touched={touched}
-                width={'100%'}
+                width={"100%"}
               />
               <FormikLabel
                 name="city"
-                onChange={(e) => setFieldValue('city', e)}
+                onChange={(e) => setFieldValue("city", e)}
                 value={values.city}
-                type={'text'}
-                title={'City'}
-                border={errors.city?.length && touched.city ? 'red' : 'white'}
+                type={"text"}
+                title={"City"}
+                border={errors.city?.length && touched.city ? "red" : "white"}
                 errors={errors}
                 touched={touched}
-                width={'100%'}
+                width={"100%"}
               />
               <FormikLabel
                 name="birthday"
-                onChange={(e) => setFieldValue('birthday', e)}
+                onChange={(e) => setFieldValue("birthday", e)}
                 value={values.birthday}
-                type={'date'}
-                title={'Date of birthday'}
-                border={errors.birthday?.length && touched.birthday ? 'red' : 'white'}
+                type={"date"}
+                title={"Date of birthday"}
+                border={errors.birthday?.length && touched.birthday ? "red" : "white"}
                 errors={errors}
                 touched={touched}
-                width={'150px'}
+                width={"150px"}
               />
               <FormikLabel
                 name="aboutMe"
-                onChange={(e) => setFieldValue('aboutMe', e)}
+                onChange={(e) => setFieldValue("aboutMe", e)}
                 value={values.aboutMe}
-                type={'textarea'}
-                title={'About Me'}
-                border={errors.aboutMe?.length && touched.aboutMe ? 'red' : 'white'}
+                type={"textarea"}
+                title={"About Me"}
+                border={errors.aboutMe?.length && touched.aboutMe ? "red" : "white"}
                 errors={errors}
                 touched={touched}
-                width={'100%'}
+                width={"100%"}
               />
               <BlockButton>
-                <StyledLine/>
-                <Button theme={ThemeButton.PRIMARY} type="submit" width={'159px'}>
+                <StyledLine />
+                <Button theme={ThemeButton.PRIMARY} type="submit" width={"159px"}>
                   Save Change
                 </Button>
               </BlockButton>
@@ -152,64 +153,58 @@ const GeneralInformation = () => {
         </Formik>
       </StyledContent>
     </SettingsPageWrapper>
-  )
-}
+  );
+};
 
-GeneralInformation.getLayout = getLayout
+GeneralInformation.getLayout = getLayout;
 export default GeneralInformation;
 
-const StyledContent = styled.div
-  `
-    position: relative;
-    display: flex;
-    gap: 40px;
+const StyledContent = styled.div`
+  position: relative;
+  display: flex;
+  gap: 40px;
 
-    @media (max-width: 790px ) {
-      flex-direction: column;
-      align-items: center;
-    }
-  `
+  @media (max-width: 790px) {
+    flex-direction: column;
+    align-items: center;
+  }
+`;
 
-const StyledAvatarBlock = styled.div
-  `
-    max-width: 192px;
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: center;
-    align-content: flex-start;
-    gap: 20px;
+const StyledAvatarBlock = styled.div`
+  max-width: 192px;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-content: flex-start;
+  gap: 20px;
 
-    background: ${baseTheme.colors.dark[700]};
-    color: ${baseTheme.colors.dark[100]};
-  `
+  background: ${baseTheme.colors.dark[700]};
+  color: ${baseTheme.colors.dark[100]};
+`;
 
-const IconBlock = styled.div
-  `
-    width: 192px;
-    height: 192px;
-    background: ${baseTheme.colors.dark[100]};
-    border-radius: 50%;
-  `
+const IconBlock = styled.div`
+  width: 192px;
+  height: 192px;
+  background: ${baseTheme.colors.dark[100]};
+  border-radius: 50%;
+`;
 
-const StyledProfileForm = styled.div
-  `
-    align-items: flex-end;
-    width: 100%;
-  `
+const StyledProfileForm = styled(Form)`
+  align-items: flex-end;
+  width: 100%;
+`;
 
-const StyledLine = styled.div
-  `
-    position: absolute;
-    bottom: 60px;
-    right: 0;
-    width: 100%;
-    max-width: 726px;
-    height: 1px;
-    background: ${baseTheme.colors.dark[300]};
-  `
+const StyledLine = styled.div`
+  position: absolute;
+  bottom: 60px;
+  right: 0;
+  width: 100%;
+  max-width: 726px;
+  height: 1px;
+  background: ${baseTheme.colors.dark[300]};
+`;
 
-const BlockButton = styled.div
-  `
-    text-align: right;
-    padding-top: 24px;
-  `
+const BlockButton = styled.div`
+  text-align: right;
+  padding-top: 24px;
+`;

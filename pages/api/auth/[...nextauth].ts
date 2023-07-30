8b26be24904ae2d1978b31261a151aff35fa2e4e
polyakog <gpolyakov77@gmail.com>
2023-07-30 21:03:18 +0400

    refac: change nextauth

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,7 +1,9 @@
-import NextAuth, { NextAuthOptions } from "next-auth";
+import NextAuth, { NextAuthOptions, User } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import GithubProvider from "next-auth/providers/github";
 import Credentials from 'next-auth/providers/credentials'
+import { signIn } from 'next-auth/react';
+import { Path } from "common/enums/path";
 
 
 // For more information on each option (and a full list of options) go to
@@ -17,9 +19,35 @@ export const authOptions: NextAuthOptions = {
     GoogleProvider({
       clientId: process.env.GOOGLE_ID!,
       clientSecret: process.env.GOOGLE_SECRET!
-    })
+    }),
+    // Credentials({
+
+    //   credentials: {
+    //     email: { label: 'loginOrEmail', type: 'text', required: true },
+    //     password: { label: 'password', type: 'password', required: true },
+    //   },
+    //   async authorize(credentials) {
+    //     if (!credentials?.email || !credentials.password) return null;
+
+    //     const currentUser = users.find(user => user.email === credentials.email)  //сделать логику
+
+    //     if (currentUser && currentUser.password === credentials.password) {
+    //       const { password, ...userWithoutPass } = currentUser;
+
+    //       return userWithoutPass as User;
+    //     }
+
+    //     return null
+    //   }
+    // })
     
   ],
+
+  pages: {
+    signIn: Path.LOGIN,
+  },
+   
+
   callbacks: {
     async jwt({ token }) {
       token.userRole = "admin";

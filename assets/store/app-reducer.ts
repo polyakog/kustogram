f6e0d76a5d74706653c656367d434c75fe6d0726
polyakog <gpolyakov77@gmail.com>
2023-08-03 18:25:45 +0400

    feat: change reducers, add selectors

--- assets/store/app-reducer.ts
+++ assets/store/app-reducer.ts
@@ -1,24 +1,59 @@
-import { createSlice, PayloadAction, ThunkAction } from "@reduxjs/toolkit";
-
+import { createSlice, PayloadAction, SerializedError, ThunkAction } from "@reduxjs/toolkit";
+import { MeType } from "./api/auth/types";
+import { FetchBaseQueryError } from "@reduxjs/toolkit/dist/query";
 
 const slice = createSlice({
-    name: "app",
-    initialState: {
-        isAppInitialized: false,
-        unhandleActions: [] as string[],
+  name: "app",
+  initialState: {
+    error: undefined as FetchBaseQueryError | SerializedError | undefined,
+    isLoading: false,
+    isAppInitialized: false,
+    me: null as MeType | null
+  },
+
+  reducers: {
+    setIsLoading: (state, action: PayloadAction<{ isLoading: boolean }>) => {
+      state.isLoading = action.payload.isLoading;
     },
-    reducers: {
-        setIsAppInitialized: (state, action: PayloadAction<{ isAppInitialized: boolean }>) => {
-            state.isAppInitialized = action.payload.isAppInitialized;
-        },
+    setError: (
+      state,
+      action: PayloadAction<{ error: FetchBaseQueryError | SerializedError | undefined }>
+    ) => {
+      state.error = action.payload.error;
     },
- 
+    setIsAppInitialized: (state, action: PayloadAction<{ isAppInitialized: boolean }>) => {
+      state.isAppInitialized = action.payload.isAppInitialized;
+    },
+    setUser: (state, action: PayloadAction<{ me: MeType }>) => {
+      state.me = action.payload.me;
+    }
+  }
+
+  // extraReducers: (builder) => {
+  //   builder
+  //   .addCase(fetchUser.pending, (state, action) => {
+  //     state.isLoading = true
+  //     state.error = null
+  //   } )
+  //   .addCase(fetchUser.fulfilled, (state, action) => {
+  //     state.isLoading = false
+  //     state.user=action.payload.user
+  //     state.isAppInitialized = action.payload.isAppInitialized
+  //   } )
+  // }
 });
 
+// export const fetchUser = createAppAsyncThunk<{ user: MeType, isAppInitialized: boolean  }, void>(
+//   Path.LOGIN,
 
+//       () => {
+//         const {data: user} = useMeQuery();
 
+//         return { user: user, isAppInitialized: true };
+
+//       }
+
+// );
 
 export const appReducer = slice.reducer;
 export const appActions = slice.actions;
-
-
---
+++ assets/store/app.selector.ts
@@ -0,0 +1,6 @@
+import { RootState } from "./store";
+
+export const isLoadingSelector = (state: RootState) => state.app.isLoading;
+export const errorSelector = (state: RootState) => state.app.error;
+export const isAppInitializedSelector = (state: RootState) => state.app.isAppInitialized;
+export const meSelector = (state: RootState) => state.app.me;

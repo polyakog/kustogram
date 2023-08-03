310b7ab90addbf163ba7e902b0ae37d7db5b88ef
polyakog <gpolyakov77@gmail.com>
2023-08-03 18:28:21 +0400

    refac: change function initializeApp

---
+++ assets/store/InitializeApp.test.ts
@@ -0,0 +1,5 @@
+import { renderHook } from "@testing-library/react";
+import { useMeQuery } from "./api/auth/authApi";
+import { appActions } from "./app-reducer";
+
+describe("my tests", () => {});
--- assets/store/InitializeApp.ts
+++ assets/store/InitializeApp.ts
@@ -1,13 +1,43 @@
-import { useAppDispatch } from "common/hooks";
 import { appActions } from "./app-reducer";
-import { useMeQuery } from "./api/auth/authApi";
+import { MeType } from "./api/auth/types";
+import { batch } from "react-redux";
+import { AppDispatch } from "./store";
+import { FetchBaseQueryError } from "@reduxjs/toolkit/dist/query";
+import { SerializedError } from "@reduxjs/toolkit";
 
+export const initializeApp = (
+  me: MeType | undefined,
+  isLoading: boolean,
+  error: FetchBaseQueryError | SerializedError | undefined,
+  dispatch: AppDispatch
+) => {
+  switch (true) {
+    case isLoading:
+      batch(() => {
+        dispatch(appActions.setIsLoading({ isLoading: true }));
+      });
+      break;
+    case !!me:
+      batch(() => {
+        // dispatch(appActions.setIsAppInitialized({ isAppInitialized: true }));
+        dispatch(appActions.setUser({ me: me! }));
+        dispatch(appActions.setIsLoading({ isLoading: false }));
+      });
+    case !error:
+      batch(() => {
+        dispatch(appActions.setError({ error: undefined }));
+        dispatch(appActions.setIsAppInitialized({ isAppInitialized: true }));
+      });
 
-export const InitializeApp = () =>  {
-const dispatch= useAppDispatch()
-    const { data: user } = useMeQuery();
-    if (!!user) {
-        dispatch(appActions.setIsAppInitialized({isAppInitialized: true}));
-    }
-        
-    }
+      break;
+    case !!error:
+      batch(() => {
+        dispatch(appActions.setError({ error: error }));
+        dispatch(appActions.setIsAppInitialized({ isAppInitialized: false }));
+        dispatch(appActions.setIsLoading({ isLoading: false }));
+      });
+
+    default:
+      break;
+  }
+};

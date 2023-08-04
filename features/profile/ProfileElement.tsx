0e6f0bf3484d0ec3cf2394634e5f566a61d76e8a
polyakog <gpolyakov77@gmail.com>
2023-08-04 18:43:49 +0400

    feat: add ProfileElement to profile page

---
+++ features/profile/ProfileElement.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+type PropsType = {
+  session: any;
+};
+
+const ProfileElement: React.FC<PropsType> = ({ session }) => {
+  return <></>;
+};
+
+export default ProfileElement;

b6ead78dba965215a0dfe63b12193ce97c5eb8fa
polyakog <gpolyakov77@gmail.com>
2023-08-07 19:43:36 +0400

    feat: split profile

--- pages/profile/index.tsx
+++ pages/profile/index.tsx
@@ -30,6 +30,7 @@ import { PostPhotos } from "features/profile/PostPhotos";
 import { useSession } from "next-auth/react";
 import { useAppSelector } from "common/hooks";
 import { isAppInitializedSelector } from "assets/store/app.selector";
+import ProfileElement from "features/profile/ProfileElement";
 
 const MyProfile = () => {
   /* _______ProtectedPage______________ */
@@ -49,13 +50,13 @@ const MyProfile = () => {
   const [isVisible, setIsVisible] = useState(true);
   const [isPaid, setIsPaid] = useState(false);
   const router = useRouter();
-  /*  ____________<переменные для мобильной версии>______________*/
+  // /*  ____________<переменные для мобильной версии>______________*/
 
-  const avatarSize = width ? (width < mediaSizes.mobileScreenSize ? 72 : 204) : 204;
-  const paidImageSize = width ? (width < mediaSizes.mobileScreenSize ? 16 : 24) : 24;
-  const postSize = width ? (width < mediaSizes.mobileScreenSize ? 108 : 228) : 228;
+  // const avatarSize = width ? (width < mediaSizes.mobileScreenSize ? 72 : 204) : 204;
+  // const paidImageSize = width ? (width < mediaSizes.mobileScreenSize ? 16 : 24) : 24;
+  // const postSize = width ? (width < mediaSizes.mobileScreenSize ? 108 : 228) : 228;
 
-  /*  ____________</переменные для мобильной версии>_______________*/
+  // /*  ____________</переменные для мобильной версии>_______________*/
 
   useEffect(() => {
     getProfileInfo();
@@ -84,81 +85,83 @@ const MyProfile = () => {
   return (
     <>
       <LoginNavigate>
-        {(session || isAppInitialized) && (
-          <ProfileWrapper>
-            <HeaderStyle>
-              {isVisible && (
-                <BlockButton>
-                  <Button
-                    theme={ThemeButton.SECONDARY}
-                    type="button"
-                    width={"auto"}
-                    style={{ padding: "6px 24px" }}
-                    onClick={handleClick}
-                  >
-                    Profile Settings
-                  </Button>
-                </BlockButton>
-              )}
-              <StyledAvatarBlock>
-                <IconBlock>
-                  <Image
-                    src={user?.photo || avatar}
-                    width={avatarSize}
-                    height={avatarSize}
-                    alt={"avatar"}
-                    // style={{ maxWidth: "204px", maxHeight: "204px" }}
-                  />
-                </IconBlock>
-              </StyledAvatarBlock>
+        {isAppInitialized && (
+          <ProfileElement user={user} posts={posts} session={session} />
 
-              <UserNameStyle>
-                {user?.firstName} {user?.lastName}
-                {isPaid && (
-                  <Image src={Paid} width={paidImageSize} height={paidImageSize} alt={"paid"} />
-                )}
-              </UserNameStyle>
+          // <ProfileWrapper>
+          //   <HeaderStyle>
+          //     {isVisible && (
+          //       <BlockButton>
+          //         <Button
+          //           theme={ThemeButton.SECONDARY}
+          //           type="button"
+          //           width={"auto"}
+          //           style={{ padding: "6px 24px" }}
+          //           onClick={handleClick}
+          //         >
+          //           Profile Settings
+          //         </Button>
+          //       </BlockButton>
+          //     )}
+          //     <StyledAvatarBlock>
+          //       <IconBlock>
+          //         <Image
+          //           src={user?.photo || avatar}
+          //           width={avatarSize}
+          //           height={avatarSize}
+          //           alt={"avatar"}
+          //           // style={{ maxWidth: "204px", maxHeight: "204px" }}
+          //         />
+          //       </IconBlock>
+          //     </StyledAvatarBlock>
 
-              <InfoBlock>
-                <FolowBlock>
-                  <div>
-                    <div>2 218</div>
-                    <div>Following</div>
-                  </div>
-                  <div>
-                    <div>2 358</div>
-                    <div>Followers</div>
-                  </div>
-                  <div>
-                    <div>2 358</div>
-                    <div>Publications</div>
-                  </div>
-                </FolowBlock>
+          //     <UserNameStyle>
+          //       {user?.firstName} {user?.lastName}
+          //       {isPaid && (
+          //         <Image src={Paid} width={paidImageSize} height={paidImageSize} alt={"paid"} />
+          //       )}
+          //     </UserNameStyle>
 
-                <AboutMeBlock>
-                  <AboutMeText>
-                    {urlify(user?.userInfo || "about me")}
+          //     <InfoBlock>
+          //       <FolowBlock>
+          //         <div>
+          //           <div>2 218</div>
+          //           <div>Following</div>
+          //         </div>
+          //         <div>
+          //           <div>2 358</div>
+          //           <div>Followers</div>
+          //         </div>
+          //         <div>
+          //           <div>2 358</div>
+          //           <div>Publications</div>
+          //         </div>
+          //       </FolowBlock>
 
-                    {/* Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-                  incididunt. laboris nisi ut aliquip ex ea commodo consequat. */}
-                  </AboutMeText>
-                </AboutMeBlock>
-              </InfoBlock>
-            </HeaderStyle>
-            {/* <PhotosBlock> */}
-            <PostPhotos posts={posts} postSize={postSize} />
+          //       <AboutMeBlock>
+          //         <AboutMeText>
+          //           {urlify(user?.userInfo || "about me")}
 
-            {/* <PhotoStyle>Photo</PhotoStyle>
-              <PhotoStyle>Photo</PhotoStyle>
-              <PhotoStyle>Photo</PhotoStyle>
-              <PhotoStyle>Photo</PhotoStyle>
-              <PhotoStyle>Photo</PhotoStyle>
-              <PhotoStyle>Photo</PhotoStyle>
-              <PhotoStyle>Photo</PhotoStyle>
-              <PhotoStyle>Photo</PhotoStyle>
-              <PhotoStyle>Photo</PhotoStyle> */}
-            {/* </PhotosBlock> */}
-          </ProfileWrapper>
+          //           {/* Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+          //         incididunt. laboris nisi ut aliquip ex ea commodo consequat. */}
+          //         </AboutMeText>
+          //       </AboutMeBlock>
+          //     </InfoBlock>
+          //   </HeaderStyle>
+          //   {/* <PhotosBlock> */}
+          //   <PostPhotos posts={posts} postSize={postSize} />
+
+          //   {/* <PhotoStyle>Photo</PhotoStyle>
+          //     <PhotoStyle>Photo</PhotoStyle>
+          //     <PhotoStyle>Photo</PhotoStyle>
+          //     <PhotoStyle>Photo</PhotoStyle>
+          //     <PhotoStyle>Photo</PhotoStyle>
+          //     <PhotoStyle>Photo</PhotoStyle>
+          //     <PhotoStyle>Photo</PhotoStyle>
+          //     <PhotoStyle>Photo</PhotoStyle>
+          //     <PhotoStyle>Photo</PhotoStyle> */}
+          //   {/* </PhotosBlock> */}
+          // </ProfileWrapper>
         )}
       </LoginNavigate>
     </>

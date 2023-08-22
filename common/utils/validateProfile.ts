d986805a1a259dfce9d11b420f2559766e350b4b
polyakog <gpolyakov77@gmail.com>
2023-08-22 13:23:22 +0400

    refac: change validate profile

--- common/utils/validateProfile.ts
+++ common/utils/validateProfile.ts
@@ -1,10 +1,14 @@
 import * as Yup from "yup";
 
 export const validateProfile = Yup.object().shape({
-  username: Yup.string().min(6, "Too Short!").max(30, "Too Long!").required("Required username"),
-  firstname: Yup.string().min(6, "Too Short!").max(20, "Too Long!").required("Required firstname"),
-  lastname: Yup.string().min(6, "Too Short!").max(20, "Too Long!").required("Required lastname"),
+  username: Yup.string()
+    .min(6, "Too Short!")
+    .max(30, "Too Long!")
+    .required("Required username")
+    .matches(/^[a-zA-Z](.[a-zA-Z0-9_-]*)$/, "invalid username"),
+  firstname: Yup.string().max(20, "Too Long!").required("Required firstname"),
+  lastname: Yup.string().max(20, "Too Long!").required("Required lastname"),
   birthday: Yup.string().required("Required birthday"),
-  city: Yup.string().min(6, "Too Short!").max(20, "Too Long!").required("Required city"),
-  aboutMe: Yup.string().min(10, "Too Short!").max(200, "Too Long!")
+  city: Yup.string().max(20, "Too Long!").required("Required city"),
+  aboutMe: Yup.string().max(200, "Too Long!")
 });

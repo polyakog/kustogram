9aff1b7f3fd1d8035a037a5763ca2f8c1a8aed99
polyakog <gpolyakov77@gmail.com>
2023-09-01 20:32:51 +0400

    feat: add selection language option modal

--- common/components/Header/SelectedLanguage/SelectLanguage.tsx
+++ common/components/Header/SelectedLanguage/SelectLanguage.tsx
@@ -1,51 +1,48 @@
 import { baseTheme } from '../../../../styles/styledComponents/theme'
-import styled from "styled-components";
-import { useRouter } from "next/router";
-import { useTranslation } from "next-i18next";
-import { useWindowSize } from "common/hooks/useWindowSize";
-import { mediaSizes } from "common/constants/Profile/mediaSizes";
-import Image from "next/image";
-import Russia from "public/img/icons/Flag-Russia.svg";
-import UK from "public/img/icons/Flag-United_Kingdom.svg";
-import { useState, useEffect } from 'react';
+import styled from 'styled-components'
+import { useRouter } from 'next/router'
+import { useTranslation } from 'next-i18next'
+import { useWindowSize } from 'common/hooks/useWindowSize'
+import { mediaSizes } from 'common/constants/Profile/mediaSizes'
+import Image from 'next/image'
+import Russia from 'public/img/icons/Flag-Russia.svg'
+import UK from 'public/img/icons/Flag-United_Kingdom.svg'
+import arrow from 'public/img/icons/arrow-ios-Down-outline-white.svg'
+import { useState, useEffect } from 'react'
 
 // Компонента выбора языка (русский и английский)
 
 const laguageOptions = {
   en: {
-    alt: "en",
+    alt: 'en',
     srs: UK,
-    text: "English"
+    text: 'English',
   },
   ru: {
-    alt: "ru",
+    alt: 'ru',
     srs: Russia,
-    text: "Русский"
-  }
+    text: 'Русский',
+  },
 }
-
-
 
 type LaguageType = typeof laguageOptions.ru
 export const SelectLanguage = () => {
-  const router = useRouter();
-  const { pathname, asPath, query } = router;
+  const router = useRouter()
+  const { pathname, asPath, query } = router
 
-  const { i18n } = useTranslation();
+  const { i18n } = useTranslation()
 
-  const { width } = useWindowSize(); // хук для измерения размера экрана
+  const { width } = useWindowSize() // хук для измерения размера экрана
   /*  ____________<переменные для мобильной версии>______________*/
 
-  const flagSize = width ? (width < mediaSizes.mobileScreenSize ? 24 : 20) : 20;
-  const [showFlagBar, setShowFlagBar] = useState(true)
-  
+  const flagSize = width ? (width < mediaSizes.mobileScreenSize ? 24 : 20) : 20
+  const [showLongBar, setShowLongBar] = useState(true)
+  const [showLongBarOption, setShowLongBarOption] = useState(true)
 
-  useEffect (()=> {
-
+  useEffect(() => {
     if (width! < mediaSizes.mobileScreenSize) {
-      setShowFlagBar(false)
-    } else setShowFlagBar(true)
-
+      setShowLongBar(false)
+    } else setShowLongBar(true)
   }, [width])
 
   /*  ____________</переменные для мобильной версии>_______________*/
@@ -53,83 +50,156 @@ export const SelectLanguage = () => {
   // Обработчик селектора языка, который перенаправляет на тот же url,
   // с теми же query параметрами, но со сменой языка
 
-  const handleLangChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
-    router.push({ pathname, query }, asPath, { locale: e.target.value })
+  const handleLangeSelect = () => {
+    if (showLongBarOption) {
+      setShowLongBarOption(false)
+    } else {
+      setShowLongBarOption(true)
+    }
   }
 
-  const handleLangeSelect = ()=> {
-
+  const handleLangChange = (selectedLanguage: string) => {
+    router.push({ pathname, query }, asPath, { locale: selectedLanguage })
+    setShowLongBarOption(false)
   }
 
-//  const [currentLanguage, setCurrentLanguage] = useState("en") 
- const currentLanguage = i18n.language == "ru" ? "ru" : "en"
-  
-let option: LaguageType
-  if (currentLanguage == "ru") {
+  const currentLanguage = i18n.language == 'ru' ? 'ru' : 'en'
+
+  let option: LaguageType
+  if (currentLanguage == 'ru') {
     option = laguageOptions.ru
-  } else {option = laguageOptions.en}
+  } else {
+    option = laguageOptions.en
+  }
 
   return (
     // <StyledSelectLanguage value={i18n.language == "ru" ? "ru" : "en"} onChange={handleLangChange}>
     //   <option value="en">&#127468;&#127463; English</option>
     //   <option value="ru">&#127479;&#127482; Русский</option>
     // </StyledSelectLanguage>
-<>  
-{ showFlagBar && (
-  <LangBox>
-    < SelectLangBlock onClick={handleLangeSelect}>
-    <Image
+    <SelectionLanguage onMouseLeave={handleLangeSelect}>
+      {showLongBar && (
+        <LangBox>
+          <SelectLangBlock onClick={handleLangeSelect}>
+            <Image priority alt={option.alt} height={flagSize} src={option.srs} width={flagSize} />
+            <LangName> {option.text}</LangName>
+            <ArrBlock>
+              <Image
+                priority
+                alt={'arrow'}
+                height={24}
+                src={arrow}
+                // style={{marginTop: "6px"}}
+                width={24}
+              />
+            </ArrBlock>
+          </SelectLangBlock>
+        </LangBox>
+      )}
+
+      {showLongBarOption && (
+        <LongBarOptionBox>
+          <OptionRow onClick={() => handleLangChange('en')}>
+            <Image
               priority
               alt={option.alt}
               height={flagSize}
-              src={option.srs}
+              src={laguageOptions.en.srs}
               width={flagSize}
             />
-            <span> {option.text}</span>
-           <div>
+            <LangName>{laguageOptions.en.text}</LangName>
+          </OptionRow>
+          <OptionRow onClick={() => handleLangChange('ru')}>
             <Image
               priority
-              alt={""}
-              height={24}
-              src={option.srs}
-              width={24}
+              alt={option.alt}
+              height={flagSize}
+              src={laguageOptions.ru.srs}
+              width={flagSize}
             />
-            </div> 
-
-
-    </SelectLangBlock>
-      
-      
-
-    </LangBox>
-)}
-
-</>
-  
+            <LangName>{laguageOptions.ru.text}</LangName>
+          </OptionRow>
+        </LongBarOptionBox>
+      )}
+    </SelectionLanguage>
   )
 }
 
 // Стили
 
+const SelectionLanguage = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 163px;
+  margin-left: 3.7%;
+  align-self: flex-start;
+  margin-top: 12px;
+  z-index: 20;
+  box-sizing: border-box;
+`
+
 const LangBox = styled.div`
+  position: relative;
+  flex-wrap: wrap;
   width: 163px;
   height: 36px;
-  flex-shrink: 0;
-  top: 12px;
-  margin-left: 3.7%;
+
   border: solid 1px ${baseTheme.colors.dark[100]};
   color: ${baseTheme.colors.light[100]};
+
+  cursor: pointer;
+  padding-left: 12px;
+`
+
+const SelectLangBlock = styled.div`
+  display: inline-flex;
+
+  width: 100%;
+  align-items: center;
+  margin-top: 6px;
+`
+
+const LangName = styled.span`
+  margin-left: 12px;
+  line-height: 24px;
   font-size: 16px;
   font-weight: 400;
   font-style: normal;
   font-family: Inter;
-  line-height: 24px;
+`
+
+const ArrBlock = styled.div`
+  position: absolute;
+  top: 6px;
+  right: 12px;
+`
+
+const LongBarOptionBox = styled.div`
+  position: relative;
+
+  width: 163px;
+  height: 72px;
+  margin-left: 0px;
+
+  color: ${baseTheme.colors.dark[900]};
+  box-sizing: border-box;
   cursor: pointer;
-`;
+`
 
-const SelectLangBlock = styled.div`
-  
+const OptionRow = styled.div`
+  display: flex;
+  align-content: center;
+  padding-top: 6px;
+  width: 163px;
+  height: 36px;
+  padding-left: 12px;
+  background-color: ${baseTheme.colors.light[100]};
 
+  &:hover {
+    background-color: ${baseTheme.colors.accent[700]};
+    color: ${baseTheme.colors.light[100]};
+    box-sizing: border-box;
+  }
 `
 
 // const StyledSelectLanguage = styled.select`

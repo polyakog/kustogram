6939cabc29ca8b874555c0b438e9f72dd03fb763
Gennadij <gpolyakov77@gmail.com>
2023-11-25 21:57:01 +0400

    feat: add data for update blocked user

--- features/admin/UserTable/MenuUserTable.tsx
+++ features/admin/UserTable/MenuUserTable.tsx
@@ -1,12 +1,15 @@
-import React, { useState } from 'react'
-import { useRouter } from 'next/router'
+import { useState } from 'react'
+
 import { useMutation } from '@apollo/client'
-import { DELETE_USER, GET_USERS } from '../../../assets/apollo/users'
-import person from '../../../public/img/icons/person.svg'
+import { ModalAdminBlockUser } from 'common/components/Modals/ModalAdmin/ModalAdminBlockUser'
+import { useRouter } from 'next/router'
+
+import { DELETE_USER, GET_USERS, UPDATE_USER_STATUS } from '../../../assets/apollo/users'
+import { ModalAdminDeleteUser } from '../../../common/components/Modals/ModalAdmin/ModalAdminDeleteUser'
 import block from '../../../public/img/icons/block_outline.svg'
 import more from '../../../public/img/icons/more-horizontal-outline.svg'
 import moreSelected from '../../../public/img/icons/more-horizontal_selected.svg'
-import { MenuPropsType } from '../types'
+import person from '../../../public/img/icons/person.svg'
 import {
   MenuIconAdmin,
   MenuItemsAdmin,
@@ -15,16 +18,12 @@ import {
   TextAdmin,
   UserMenuAdmin,
 } from '../Admin.styled'
-import Modal from '../../../common/components/Modals/ModalPublic/Modal'
-import { ModalBtn } from '../../../styles/styledComponents/acc_management/acc_management.styled'
-import { Button } from '../../../common/components/Button/Button'
-import { ThemeButton } from '../../../common/enums/themeButton'
-import { LogoutModal } from '../../../common/components/Navbar/LogoutLink/logoutLink'
-import { ModalAdminDeleteUser } from '../../../common/components/Modals/ModalAdmin/ModalAdminDeleteUser'
+import { MenuPropsType } from '../types'
 
-export const MenuUserTable = ({ id }: MenuPropsType) => {
+export const MenuUserTable = ({ id, ban, userName }: MenuPropsType) => {
   const [isActive, setIsActive] = useState(false)
   const [isOpenModalDelete, setIsOpenModalDelete] = useState<boolean>(false)
+  const [isOpenModalBlock, setIsOpenModalBlock] = useState<boolean>(false)
 
   const handleSelect = () => {
     setIsActive(prev => !prev)
@@ -39,9 +38,13 @@ export const MenuUserTable = ({ id }: MenuPropsType) => {
     push(`/admin/${userId}`)
   }
 
-  const blockUser = () => {
-    console.log('blocked')
-  }
+  const [blockUser] = useMutation(UPDATE_USER_STATUS, {
+    variables: {
+      userId: id,
+      banStatus: !ban,
+    },
+    refetchQueries: [GET_USERS, 'GetUsers'],
+  })
 
   const [deleteUser, { loading }] = useMutation(DELETE_USER, {
     variables: {
@@ -52,7 +55,7 @@ export const MenuUserTable = ({ id }: MenuPropsType) => {
 
   const menu = [
     { src: person, alt: 'icon', text: 'Delete User', handler: deleteUser },
-    { src: block, alt: 'icon', text: 'Ban in the system', handler: blockUser },
+    { src: block, alt: 'icon', text: ban ? 'Un-ban' : 'Ban in the system', handler: blockUser },
     { src: more, alt: 'icon', text: 'More Information', handler: handleNavigate },
   ]
 
@@ -72,6 +75,8 @@ export const MenuUserTable = ({ id }: MenuPropsType) => {
               onClick={() => {
                 if (item.text === 'Delete User') {
                   setIsOpenModalDelete(true)
+                } else if (item.text === 'Ban in the system' || item.text === 'Un-ban') {
+                  setIsOpenModalBlock(true)
                 } else item.handler(id)
               }}
             >
@@ -86,6 +91,15 @@ export const MenuUserTable = ({ id }: MenuPropsType) => {
           id={id}
           isOpenModalDelete={isOpenModalDelete}
           setIsOpenModalDelete={setIsOpenModalDelete}
+        />
+      )}
+      {isOpenModalBlock && (
+        <ModalAdminBlockUser
+          ban={ban}
+          id={id}
+          isOpenModalBlock={isOpenModalBlock}
+          setIsOpenModalBlock={setIsOpenModalBlock}
+          userName={userName}
         />
       )}
     </UserMenuAdmin>
